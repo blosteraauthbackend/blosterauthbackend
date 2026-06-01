ce6b022927cb422a1a72f9d2f3ee6a46091848cf
sonyaadmin120 <sonyatiktok120@gmail.com>
2026-06-01 08:28:34 +0500

    mnkwdl

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { createClient } = require("@libsql/client");
 
 if (!process.env.TURSO_USER_URL || !process.env.TURSO_USER_TOKEN) {
-    console.error("❌ ERROR: TURSO_USER_URL ya TURSO_USER_TOKEN .env mein nahi hai!");
+    console.error("❌ ERROR:  g tTURSO_USER_URL ya TURSO_USER_TOKEN .env mein nahi hai!");
 }
 
 const userDB = createClient({
